d4582ed7c920f717725c0324d54a7d9add9c6e91
Islamdev2025 <okurmenstudio2025@gmail.com>
2026-01-13 03:04:06 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
 
     try {
       const my_id = `-5104446329`;
-      const token = `8432212243:AAFGvxAVNUbL2hqdTLBAqapPTgBXNaQArWs`;
+      const token = `-8432212243:AAFGvxAVNUbL2hqdTLBAqapPTgBXNaQArWs`;
       const api_key = `https://api.telegram.org/bot${token}/sendMessage`;
 
       const userData = {
